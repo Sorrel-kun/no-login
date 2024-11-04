1192f348a5e265b430600461439c3cd5ababd0b0
MartinBraquet <martin.braquet@gmail.com>
2024-11-04 01:35:57 +0100

    Fix test

--- .github/workflows/run_tests.sh
+++ .github/workflows/run_tests.sh
@@ -2,12 +2,6 @@ set -e
 
 sudo apt install npm
 # npm install web-ext
-npm install -g mocha
-npm install --save-dev @babel/register
-npm list @babel/register
 npm install .
 npm list
-
-echo $PATH
-
 npm test
