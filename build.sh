b8e40c7d1937f0eb8befa1ff2aaca1674ed0ec88
MartinBraquet <martin.braquet@gmail.com>
2024-11-04 20:34:06 +0100

    Update build

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-web-ext build --overwrite-dest --ignore-files demo martin readme misc package-lock.json release.sh build.sh store tests
+web-ext build --overwrite-dest --ignore-files demo martin readme misc package-lock.json release.sh build.sh store test coverage
