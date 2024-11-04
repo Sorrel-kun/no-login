066ca84cb50dcfe4aa36ab4a5f78cc9ed4a7532c
MartinBraquet <martin.braquet@gmail.com>
2024-11-04 19:18:55 +0100

    Auto-launch URLs from all websites for quick verification

---
+++ web-ext-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  verbose: true,
+  build: {overwriteDest: true},
+  run: {
+    startUrl: [
+        "https://www.facebook.com/nature.be/?locale=fr_BE",
+        "https://www.facebook.com/nature.be/",
+        "https://www.linkedin.com/company/the-nature-conservancy",
+        "https://www.instagram.com/nature_org/",
+        "https://www.instagram.com/nature_org/?hl=fr",
+    ],
+  },
+};
