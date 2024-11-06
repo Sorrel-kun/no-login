5077d507584b78c6cf14e3e4d7167e46de811f33
MartinBraquet <martin.braquet@gmail.com>
2024-11-06 19:07:12 +0100

    Echo

--- sign.sh
+++ sign.sh
@@ -6,4 +6,5 @@ if [ -z "$AMO_JWT_ISSUER" ]; then
   source /home/martin/Documents/Passwords/amo_creds.sh
 fi
 
+echo "Signing extension..."
 web-ext sign --api-key $AMO_JWT_ISSUER --api-secret $AMO_JWT_SECRET
